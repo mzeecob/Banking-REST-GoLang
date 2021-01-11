924f486b83c1b33a0a03bb3dbc77ede0ef93dd33
mzeecob <reshimwe2025@gmail.com>
2021-01-12 00:00:25 +0200

    firs go webserver app

--- Main.go
+++ Main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"fmt"
 )
 
+func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "<h1> bimeze gte its ur boy <h1>")
+}
+
 func main() {
-	i := "vip mzee"
-	fmt.Println(i)
-	fmt.Println ("hello Go!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
