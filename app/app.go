2e619f24c5381ed447c9552fb54024dd7d0c1fd4
mzeecob <reshimwe2025@gmail.com>
2021-01-25 21:50:35 +0200

    gorila mux, a routing tool

--- app/app.go
+++ app/app.go
@@ -1,9 +1,31 @@
 package app
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 func Start()  {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/customers", getAllCustomer)
-	http.ListenAndServe(":3000", nil)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", handlerFunc).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post method received")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	 vars := mux.Vars(r)
+	 fmt.Fprint(w, vars["customer_id"])
 }
